cee1866b54b2e7e2b463e3f7579a70629c01022c
Victor Hoang <victorho@usc.edu>
2024-04-22 22:57:45 -0700

    Add main layout, css, fonts, and images

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="@yield('description')">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;1,700&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="{{ asset('fonts/font-awesome.min.css') }}" rel="stylesheet">
+
+    <title>@yield('title')</title>
+
+    <style>
+        @yield('style')
+    </style>
+</head>
+
+<body>
+    @yield('main')
+</body>
+</html>
