f558a6c01708a28b29faf7b50e235c83d06b8afc
Victor Hoang <victorho@usc.edu>
2024-04-22 22:22:52 -0700

    Add migrations for database tables

---
+++ database/migrations/2024_04_23_051718_create_restaurants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->nullable(false);
+            $table->string('address', 255)->nullable(false);
+            $table->string('phone_number', 255)->nullable();
+            $table->string('cuisine', 255)->nullable();
+            $table->double('rating')->nullable();
+            $table->string('price', 255)->nullable();
+            $table->string('url', 255)->nullable();
+            $table->string('image_url', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
